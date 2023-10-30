1efa6cc45919ca17e700a858a18550e247ad5297
akunerlia@gmail.com <akunerlia@gmail.com>
2023-10-30 10:02:29 +0700

    Membuat project baru

---
+++ src/aplikasi/penjualan/neatbeans_k11/AplikasiPenjualanNeatbeans_K11.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi.penjualan.neatbeans_k11;
+
+/**
+ *
+ * @author lab-4
+ */
+public class AplikasiPenjualanNeatbeans_K11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
